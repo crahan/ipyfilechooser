64a00e87f8e73780908e614119be2beaf579a32c
Thomas Bouve <crahan@n00.be>
2019-11-28 01:59:08 +0100

    Add read function

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def read(fname):
+    """Open files relative to package."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name="ipyfilechooser",
@@ -13,7 +17,7 @@ setup(
         "Python file chooser widget for use in "
         "Jupyter/IPython in conjunction with ipywidgets"
     ),
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
     url="https://github.com/crahan/ipyfilechooser",
     license="MIT",

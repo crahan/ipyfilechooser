af1c1212c9af0cb07878c114a27da4596260e66a
Thomas Bouve <crahan@n00.be>
2019-12-03 02:46:32 +0100

    extend vs append.

--- ipyfilechooser/utils.py
+++ ipyfilechooser/utils.py
@@ -18,7 +18,7 @@ def get_subpaths(path):
 
     try:
         # Add Windows drive letters, but remove the current drive
-        paths.append(get_drive_letters())
+        paths.extend(get_drive_letters().remove(paths[-1]))
     except ValueError:
         pass
     return paths
